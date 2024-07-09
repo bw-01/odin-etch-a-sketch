bdc4ff148bdbf68636093df45d1eaee2f2e8cb6d
BW <17097491+bw-01@users.noreply.github.com>
2024-07-09 14:34:25 +1200

    Added random colour functionality, clear button and styling.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Etch-a-sketch from The Odin Project
 A basic sketchpad app using HTML, JavaScript and CSS with Flexbox.
+
 Author: BW-01
 
 ## Instructions
-Move your mouse around to draw
+- Run the Github Live Page from here: 
+- Move your mouse around to draw.
+- Click the 'Width/Height' button to input new width/height dimensions between 2 and 100 squares.
+- Click the 'Randomise colour' button to switch between randomly coloured and black ink.
+- Click 'Clear grid' to clear the contents of the grid.
--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let gridDimensions = 16;
 const CANVAS_SIZE = 960;
+let useColours = false;
 
 const container = document.querySelector(".container");
 const dimensions = document.querySelector(".dimensionsButton");
+const randomise = document.querySelector(".colourButton");
+const clear = document.querySelector(".clearButton");
 
 createGrid();
 
 container.addEventListener("mouseover", (event) => {
   if (event.target.classList.contains("square")) {
-    event.target.style.background = "black";
+    if (!useColours) {
+      event.target.style.background = "black";
+    } else {
+      event.target.style.background = `rgb(${Math.floor(Math.random() * 256)}, 
+      ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    }
   }
 });
 
@@ -17,11 +25,21 @@ dimensions.addEventListener("click", (event) => {
     gridDimensions = prompt("Enter grid dimensions [between 2 and 100]");
   } while (gridDimensions < 2 || gridDimensions > 100);
 
+  dimensions.textContent = "Width/Height: " + gridDimensions;
+  createGrid();
+});
+
+randomise.addEventListener("click", (event) => {
+  useColours = !useColours;
+  randomise.textContent = useColours ? "Back to black" : "Randomise colours";
+});
+
+clear.addEventListener("click", () => {
   createGrid();
 });
 
 function createGrid() {
-  container.innerHTML = ''; //clear the grid
+  container.innerHTML = ""; //clear the grid
 
   for (i = 0; i < gridDimensions * gridDimensions; i++) {
     squareSize = CANVAS_SIZE / gridDimensions;
