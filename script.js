15eef5e22da5c6e592753db9543c34992069a77e
BW <17097491+bw-01@users.noreply.github.com>
2024-07-09 13:30:06 +1200

    Finished basic functionality with user option to set dimensions.

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-let gridSize = 16;
+let gridDimensions = 16;
 const CANVAS_SIZE = 960;
 
 const container = document.querySelector(".container");
+const dimensions = document.querySelector(".dimensionsButton");
 
-for (i = 0; i < gridSize * gridSize; i++) {
-  squareSize = CANVAS_SIZE / gridSize;
-  const gridDiv = document.createElement("div");
-  gridDiv.classList.add("gridDiv");
-  gridDiv.style.width = `${squareSize}px`;
-  gridDiv.style.height = `${squareSize}px`;
-  container.appendChild(gridDiv);
+createGrid();
+
+container.addEventListener("mouseover", (event) => {
+  if (event.target.classList.contains("square")) {
+    event.target.style.background = "black";
+  }
+});
+
+dimensions.addEventListener("click", (event) => {
+  do {
+    gridDimensions = prompt("Enter grid dimensions [between 2 and 100]");
+  } while (gridDimensions < 2 || gridDimensions > 100);
+
+  createGrid();
+});
+
+function createGrid() {
+  container.innerHTML = ''; //clear the grid
+
+  for (i = 0; i < gridDimensions * gridDimensions; i++) {
+    squareSize = CANVAS_SIZE / gridDimensions;
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add("square");
+    squareDiv.style.width = `${squareSize}px`;
+    squareDiv.style.height = `${squareSize}px`;
+    container.appendChild(squareDiv);
+  }
 }
